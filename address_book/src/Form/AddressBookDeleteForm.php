d47fe42e48f7dae34451260fdf6e511b9f41dd7b
kseroff <112875767+kseroff@users.noreply.github.com>
2023-05-12 18:11:01 +0300

    Add files via upload

---
+++ address_book/src/Form/AddressBookDeleteForm.php
@@ -0,0 +1,43 @@
+<?php
+namespace Drupal\address_book\Form;
+
+use Drupal\Core\Entity\ContentEntityConfirmFormBase;
+use Drupal\Core\Url;
+
+class AddressBookDeleteForm extends ContentEntityConfirmFormBase {
+
+  public function getCancelURL() {
+    return new Url('address_book.list');
+  }
+
+  public function getQuestion() {
+    return $this->t('Do you want to delete the contact %name?', ['%name' => $this->entity->get('field_full_name')->value]);
+  }
+
+  public function getDescription() {
+    return $this->t('This action cannot be undone.');
+  }
+
+  public function getConfirmText() {
+    return $this->t('Delete');
+  }
+
+  public function getEntityTypeId() {
+    return 'address_book';
+  }
+
+  public function getFormId() {
+    return 'address_book.delete_form';
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $entity = $this->entity;
+    $entity->delete();
+
+    drupal_set_message($this->t('Contact has been deleted.'));
+    
+    //список контактов перенаправил. наверно
+    $form_state->setRedirect('address_book.list');
+  }
+
+}

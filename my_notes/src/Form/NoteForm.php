0391c39827b0f0fc2f4448eac92ccda458d8cb46
kseroff <112875767+kseroff@users.noreply.github.com>
2023-05-23 22:30:53 +0300

    Add files via upload

--- my_notes/src/Form/NoteForm.php
+++ my_notes/src/Form/NoteForm.php
@@ -1,11 +1,14 @@
 <?php
+
 namespace Drupal\my_notes\Form;
 
 use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Form controller for the note entity edit forms.
+ * Form controller for the note edit form.
+ *
+ * @ingroup my_notes
  */
 class NoteForm extends ContentEntityForm {
 
@@ -15,10 +18,21 @@ class NoteForm extends ContentEntityForm {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form = parent::buildForm($form, $form_state);
 
-    $form['body']['#type'] = 'text_format';
-    $form['body']['#format'] = 'full_html';
+    // Add custom form elements here, if needed.
 
     return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function save(array $form, FormStateInterface $form_state) {
+    $entity = $this->getEntity();
+    $entity->set('author', \Drupal::currentUser()->id());
+    $entity->save();
+
+    drupal_set_message($this->t('The note has been saved.'));
+    $form_state->setRedirect('entity.note.canonical', ['note' => $entity->id()]);
   }
 
 }

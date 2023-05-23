e1e17f0bf1e28f02cbf2ce21767bb191185163b4
kseroff <112875767+kseroff@users.noreply.github.com>
2023-05-23 22:28:15 +0300

    Add files via upload

--- address_book/src/Controller/AddressBookController.php
+++ address_book/src/Controller/AddressBookController.php
@@ -12,35 +12,51 @@ class AddressBookController extends ControllerBase {
   public function list() {
     // Получить список контактов
     $contacts = \Drupal::entityTypeManager()->getStorage('address_book')->loadMultiple();
-  
+
     // Создание таблицы
     $header = [
       'id' => $this->t('ID'),
-      'field_full_name' => $this->t('Full Name'),
-      'field_phone_number' => $this->t('Phone Number'),
-      'field_job_title' => $this->t('Job Title'),
-      'edit' => $this->t('Edit'),
-      'delete' => $this->t('Delete'),
+      'field_full_name' => $this->t('Полное имя'),
+      'field_phone_number' => $this->t('Номер телефона'),
+      'field_job_title' => $this->t('Должность'),
+      'options' => $this->t('Опции'),
     ];
-  
+
+      // Создание кнопки "Add Contact"
+      $addLink = Link::fromTextAndUrl($this->t('Добавь Контакт'), Url::fromRoute('address_book.add'));
+      $addLink = $addLink->toRenderable();
+      $addLink['#attributes'] = ['class' => ['button']];
+      $addLink = ['#markup' => render($addLink)];
+
     $rows = [];
-  
+
     foreach ($contacts as $contact) {
       $rows[] = [
         'id' => $contact->id(),
         'field_full_name' => $contact->get('field_full_name')->value,
         'field_phone_number' => $contact->get('field_phone_number')->value,
         'field_job_title' => $contact->get('field_job_title')->value,
-        'edit' => Link::fromTextAndUrl($this->t('Edit'), Url::fromRoute('address_book.edit', ['id' => $contact->id()])),
-        'delete' => Link::fromTextAndUrl($this->t('Delete'), Url::fromRoute('address_book.delete', ['id' => $contact->id()])),
+        'options' => [
+          'data' => [
+            '#type' => 'dropbutton',
+            '#links' => [
+              'edit' => [
+                'title' => $this->t('Изменить'),
+                'url' => Url::fromRoute('address_book.edit', ['id' => $contact->id()]),
+              ],
+              'delete' => [
+                'title' => $this->t('Удалить'),
+                'url' => Url::fromRoute('address_book.delete', ['id' => $contact->id()]),
+              ],
+            ],
+            '#attributes' => ['class' => ['options']],
+            '#attached' => [
+              'library' => ['core/drupal.dropbutton'],
+            ],
+          ],
+        ],
       ];
     }
-  
-    // Создание кнопки "Add Contact"
-    $addLink = Link::fromTextAndUrl($this->t('Add Contact'), Url::fromRoute('address_book.add'));
-    $addLink = $addLink->toRenderable();
-    $addLink['#attributes'] = ['class' => ['button']];
-    $addLink = ['#markup' => render($addLink)];
 
     // Создание таблицы
     $table = [
@@ -50,10 +66,10 @@ class AddressBookController extends ControllerBase {
       '#empty' => $this->t('There are no contacts yet.'),
     ];
 
-    // Создание строки поиска
-    $searchForm = \Drupal::formBuilder()->getForm('\Drupal\address_book\Form\SearchForm');
-    $searchForm['#prefix'] = render($addLink);
-    $searchForm['#suffix'] = render($table);
+     // Создание строки поиска
+     $searchForm = \Drupal::formBuilder()->getForm('\Drupal\address_book\Form\SearchForm');
+     $searchForm['#prefix'] = render($addLink);
+     $searchForm['#suffix'] = render($table);
 
     return $searchForm;
   }
@@ -89,6 +105,5 @@ class AddressBookController extends ControllerBase {
     $form = \Drupal::formBuilder()->getForm('\Drupal\address_book\Form\AddressBookFormEdit', $id);
     return $form;
   }
-
 
 }

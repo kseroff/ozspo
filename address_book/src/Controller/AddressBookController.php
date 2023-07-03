66c4b6f51cb25e1b249416b2255cc5ad11bd1948
kseroff <112875767+kseroff@users.noreply.github.com>
2023-07-03 21:07:47 +0300

    Add files via upload

--- address_book/src/Controller/AddressBookController.php
+++ address_book/src/Controller/AddressBookController.php
@@ -4,6 +4,8 @@ namespace Drupal\address_book\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\Request;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Link;
 use Drupal\Core\Url;
 
@@ -15,7 +17,7 @@ class AddressBookController extends ControllerBase {
     $query->sort('field_full_name', 'ASC');
     $contact_ids = $query->execute();
     $contacts = \Drupal::entityTypeManager()->getStorage('address_book')->loadMultiple($contact_ids);
-
+  
     // Создание таблицы
     $header = [
       'id' => $this->t('ID'),
@@ -27,33 +29,35 @@ class AddressBookController extends ControllerBase {
       'field_address' => $this->t('Адрес'),
       'options' => $this->t('Опции'),
     ];
-
+  
     // Создание кнопки "Add Contact"
     $addLink = Link::fromTextAndUrl($this->t('Добавить Контакт'), Url::fromRoute('address_book.add'));
     $addLink = $addLink->toRenderable();
     $addLink['#attributes'] = ['class' => ['button']];
     $addLink = ['#markup' => render($addLink)];
-
+  
     $rows = [];
-
+  
     foreach ($contacts as $contact) {
-    
+      $author = $contact->get('field_author')->entity;
+      $author_name = $author ? $author->getDisplayName() : '';
+  
       $department = $contact->get('field_department')->entity;
       $department_name = $department ? $department->getName() : '';
-    
+  
+      $is_personal = $contact->get('field_personal')->value;
+      $has_access = $this->accessCheck(\Drupal::currentUser())->isAllowed();
+  
       $rows[] = [
         'id' => $contact->id(),
         'field_full_name' => $contact->get('field_full_name')->value,
         'field_phone_number' => $contact->get('field_phone_number')->value,
         'field_job_title' => $contact->get('field_job_title')->value,
-        'field_department' => [
-          'data' => [
-            '#type' => 'link',
-            '#title' => $department_name,
-            '#url' => Url::fromRoute('entity.taxonomy_term.canonical', ['taxonomy_term' => $department->id()]),
-          ],
-        ],
-        'field_personal' => $contact->get('field_personal')->value ? $this->t('Да') : $this->t('Нет'),
+        'field_author' => $author_name,
+        'field_created_date' => \Drupal::service('date.formatter')->format($contact->getCreatedTime()),
+        'field_modified_date' => \Drupal::service('date.formatter')->format($contact->getChangedTime()),
+        'field_department' => $department_name,
+        'field_personal' => $is_personal ? $this->t('Да') : $this->t('Нет'),
         'field_address' => $contact->get('field_address')->value,
         'options' => [
           'data' => [
@@ -62,10 +66,12 @@ class AddressBookController extends ControllerBase {
               'edit' => [
                 'title' => $this->t('Редактировать'),
                 'url' => Url::fromRoute('address_book.edit', ['id' => $contact->id()]),
+                'access' => $has_access && !$is_personal,
               ],
               'delete' => [
                 'title' => $this->t('Удалить'),
                 'url' => Url::fromRoute('address_book.delete', ['id' => $contact->id()]),
+                'access' => $has_access && !$is_personal,
               ],
             ],
             '#attributes' => ['class' => ['options']],
@@ -74,9 +80,10 @@ class AddressBookController extends ControllerBase {
             ],
           ],
         ],
+        'access' => $has_access || !$is_personal,
       ];
     }
-
+  
     // Создание таблицы
     $table = [
       '#type' => 'table',
@@ -84,20 +91,29 @@ class AddressBookController extends ControllerBase {
       '#rows' => $rows,
       '#empty' => $this->t('Контактов пока нет.'),
     ];
-
+  
     // Создание формы поиска
     $searchForm = \Drupal::formBuilder()->getForm('\Drupal\address_book\Form\SearchForm');
     $searchForm['#prefix'] = render($addLink);
     $searchForm['#suffix'] = '<div id="address-book-table">' . render($table) . '</div>';
-
+  
     // Добавление строки поиска и кнопки добавления
     $output = [
       '#prefix' => '<div class="address-book-wrapper">',
       '#suffix' => '</div>',
     ];
     $output['search_form'] = $searchForm;
-
+  
     return $output;
+  }
+
+  protected function accessCheck(AccountInterface $account) {
+    // Проверка на наличие прав "administer address_book content".
+    if ($account->hasPermission('administer address_book content')) {
+      return AccessResult::allowed();
+    }
+  
+    return AccessResult::neutral();
   }
 
   public function delete($id) {
